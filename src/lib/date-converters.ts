9bd2ce657fcb6cdb176508b214439709c1d5c84b
Raphael Hue <r.hue@agrial.com>
2022-11-30 16:22:10 +0100

    remove dateconverter

--- src/lib/date-converters.ts
+++ src/lib/date-converters.ts
@@ -1,22 +1,10 @@
 import { AbstractJsonConverter, Context } from './json-converter'
 
 export class DateTimeConverter extends AbstractJsonConverter<string, Date>{
-  serialize(obj: Date, context?: Context): string {
+  serialize (obj: Date, context?: Context): string {
     return obj.toISOString();
   }
-  deserialize(obj: string, context?: Context): Date {
+  deserialize (obj: string, context?: Context): Date {
     return new Date(Date.parse(obj));
-  }
-}
-
-export class DateConverter extends AbstractJsonConverter<string, Date>{
-  serialize(obj: Date, context?: Context): string {
-    return obj.toISOString().substring(0, 10);
-  }
-  deserialize(obj: string, context?: Context): Date {
-    const date = new Date(Date.parse(obj));
-    date.setUTCHours(0, 0, 0, 0);
-
-    return date;
   }
 }
